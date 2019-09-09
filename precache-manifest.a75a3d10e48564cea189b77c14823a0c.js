835c7fa9a73419f43b17030054b03767365c8ed3
Sahar Elsayed <Sahar.Elsayed@ibtikar.com>
2019-09-09 11:34:03 +0200

    Updates

---
+++ precache-manifest.a75a3d10e48564cea189b77c14823a0c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcce3532106fd25a0494de919d7a9ef3",
+    "url": "/Songs-Redux-CRUD/index.html"
+  },
+  {
+    "revision": "ff79183918f739aff152",
+    "url": "/Songs-Redux-CRUD/static/js/2.8a48a680.chunk.js"
+  },
+  {
+    "revision": "74ef25b6d5508743df0a",
+    "url": "/Songs-Redux-CRUD/static/js/main.b83ec664.chunk.js"
+  },
+  {
+    "revision": "3d6c0f3821ffad97a6bd",
+    "url": "/Songs-Redux-CRUD/static/js/runtime~main.702ec721.js"
+  }
+]);
